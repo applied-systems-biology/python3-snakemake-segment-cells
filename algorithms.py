e3df576203e08449aa01c7aef2c567558b48b110
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-11-13 16:47:40 +0100

    * synchronized with other implementations

--- algorithms.py
+++ algorithms.py
@@ -5,6 +5,7 @@ from scipy import ndimage as ndi
 from skimage import filters
 from skimage import (img_as_ubyte, img_as_float, img_as_int)
 from skimage import io
+from skimage import color
 import skimage.morphology as morph
 from skimage.morphology import watershed
 import json
@@ -18,11 +19,13 @@ def segment_conidia(input_file, output_file):
     thresholded = mahotas.close_holes(thresholded)
 
     distance = ndi.distance_transform_edt(thresholded)
-    local_maxi = distance == morph.dilation(distance, morph.square(3))
+    local_maxi = distance == morph.dilation(distance, morph.square(5))
     local_maxi[thresholded == 0] = 0
     markers = ndi.label(local_maxi)[0]
-    labels = watershed(-distance, markers, mask=thresholded)
+
+    labels = watershed(thresholded, markers, mask=thresholded)
     tifffile.imsave(output_file, img_as_int(labels), compress=5)
+    # tifffile.imsave(output_file + ".vis.tif", img_as_ubyte(color.label2rgb(labels, bg_label=0)), compress=5)
 
 
 def quantify_conidia(label_dir, output_file, experiments):
